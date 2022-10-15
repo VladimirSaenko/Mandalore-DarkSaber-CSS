a9933d5e9a6158c2df9577e55b1ba629fd488fae
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2022-10-15 21:32:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ function getRandomColor() {
     document.documentElement.style.setProperty('--blade-color', `${getRandomInt(10, 1000)}`);
 }
 
-document.getElementById("darksaber").addEventListener('click', () => {
-    document.getElementById("title").classList.toggle('active');
-})
+getRandomColor();
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-getRandomColor();
+document.getElementById("darksaber").addEventListener('click', () => {
+    document.getElementById("title").classList.toggle('active');
+})
